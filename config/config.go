9d4cdff1a0e1ea0ae30f71fac10d5dc44a608087
ofree8 <blusky719@163.com>
2018-08-30 17:43:43 +0800

    modify config

--- config/config.go
+++ config/config.go
@@ -31,6 +31,7 @@ func Init(cfg string) error {
 	return nil
 }
 
+// init all config files
 func (c *Config) initConfig() error {
 	if c.Name != "" {
 		viper.SetConfigFile(c.Name) // 如果指定了配置文件，则解析指定的配置文件
@@ -38,9 +39,9 @@ func (c *Config) initConfig() error {
 		viper.AddConfigPath("conf") // 如果没有指定配置文件，则解析默认的配置文件
 		viper.SetConfigName("config")
 	}
-	viper.SetConfigType("yaml")     // 设置配置文件格式为YAML
-	viper.AutomaticEnv()            // 读取匹配的环境变量
-	viper.SetEnvPrefix("APISERVER") // 读取环境变量的前缀为APISERVER
+	viper.SetConfigType("yaml")       // 设置配置文件格式为YAML
+	viper.AutomaticEnv()              // 读取匹配的环境变量
+	viper.SetEnvPrefix("SERVICEBASE") // 读取环境变量的前缀为 SERVICEBASE
 	replacer := strings.NewReplacer(".", "_")
 	viper.SetEnvKeyReplacer(replacer)
 	if err := viper.ReadInConfig(); err != nil { // viper解析配置文件
--- core/base.go
+++ core/base.go
@@ -4,8 +4,8 @@ import (
 	"github.com/ofree8/service-base/config"
 )
 
-func main() {
-	if err := config.Init("Service-base"); err != nil {
+func init() {
+	if err := config.Init("service-base"); err != nil {
 
 	}
 }
