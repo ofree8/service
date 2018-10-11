05b77284e74ea15be8d91ecafd26dea149782809
ofree8 <blusky719@163.com>
2018-10-11 14:18:11 +0800

    ddd

--- db.sql
+++ db.sql
@@ -33,6 +33,44 @@ CREATE TABLE `tb_users` (
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+
+--
+-- 表的结构 `user`
+--
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `email` char(32) NOT NULL DEFAULT '' COMMENT '电子邮箱',
+  `username` char(20) NOT NULL DEFAULT '' COMMENT '用户名',
+  `password` char(32) NOT NULL DEFAULT '' COMMENT '密码',
+  `encrypt` char(6) NOT NULL DEFAULT '' COMMENT '随机码',
+  `nickname` char(20) NOT NULL DEFAULT '' COMMENT '昵称',
+  `mobile` char(11) NOT NULL DEFAULT '' COMMENT '手机',
+  `birthday` date NOT NULL DEFAULT '0000-00-00' COMMENT '生日',
+  `regip` char(15) NOT NULL DEFAULT '' COMMENT '注册ip',
+  `regdate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '注册时间',
+  `lastdate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '最后登录时间',
+  `lastip` char(15) NOT NULL DEFAULT '' COMMENT '上次登录ip',
+  `loginnum` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '登陆次数',
+  `groupid` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '用户组id',
+  `areaid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '地区id',
+  `amount` decimal(8,2) unsigned NOT NULL DEFAULT '0.00' COMMENT '金钱总额',
+  `point` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '积分',
+  `ismessage` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否有短消息',
+  `islock` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否定锁',
+  `vip` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'vip等级',
+  `overduedate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'vip过期时间',
+  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '审核状态 5:用户名已存在;4:拒绝;3:删除:2:忽略;0:未审核;1:通过',
+  `createtime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  KEY `email` (`email`(20))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+
+
+
+
 --
 -- Dumping data for table `tb_users`
 --
